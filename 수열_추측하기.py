f4e457859fba69af9ba7035d6509b3a12709fdda
Kim Jihoon <max.kim@ebridge-world.com>
2022-07-03 23:52:59 +0900

    순열_구하기 fix

--- 수열_추측하기.py
+++ 수열_추측하기.py
@@ -1,21 +1,18 @@
-def DFS_pick(L, n, m, cur):
-    print(L, n, m)
-    print(res)
-    if L == m:
+def DFS(L, n, cur):
+    if L == n:
         temp.append(res[:])
     else:
         for i in range(0, n):
             if i + 1 != cur:
                 res[L] = i + 1
-                DFS_pick(L + 1, n, m, i + 1)
+                DFS(L + 1, n, i + 1)
 
 
 if __name__ == '__main__':
     # main script 에 생성되면 전역변수
-    n, m = 3, 2
+    n, m = 4, 16
     temp = list()
-    res = [0] * m
-    DFS_pick(0, n, m, 0)
+    res = [0] * n
+    DFS(0, n, 0)
     print(temp)
-    print(len(temp))
 
--- 순열_구하기.py
+++ 순열_구하기.py
@@ -5,14 +5,14 @@ def DFS_pick(L, n, m, cur):
         temp.append(res[:])
     else:
         for i in range(0, n):
-            if i + 1 != cur:
+            if i + 1 != cur and res[L] == 0:
                 res[L] = i + 1
                 DFS_pick(L + 1, n, m, i + 1)
 
 
 if __name__ == '__main__':
     # main script 에 생성되면 전역변수
-    n, m = 3, 2
+    n, m = 3, 3
     temp = list()
     res = [0] * m
     DFS_pick(0, n, m, 0)
