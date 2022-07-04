454cc4eacf8a2ff9b54c37362ae63b38c8e2f669
Kim Jihoon <max.kim@ebridge-world.com>
2022-07-05 00:29:09 +0900

    조합 구하기

--- 조합_구하기.py
+++ 조합_구하기.py
@@ -1,20 +1,21 @@
-def DFS(L, n, m):
+def DFS(L, n, m, num):
     if L == m:
-        print(temp)
-        res.append(temp)
+        res.append(sub_res[:])
     else:
-        for i in range(0, n):
-            if temp[L] == 0:
-                temp[L] = i + 1
-                DFS(L + 1, n, m)
-                temp[L] = 0
+        for i in range(num, n):
+            if temp[i] == 0:
+                temp[i] = 1
+                sub_res[L] = i + 1
+                DFS(L + 1, n, m, i + 1)
+                temp[i] = 0
 
 
 if __name__ == '__main__':
     # main script 에 생성되면 전역변수
-    n, m = 4, 2
-    temp = [0] * m
+    n, m = 7, 4
+    temp = [0] * n
     res = list()
-    DFS(0, n, m)
+    sub_res = [0] * m
+    DFS(0, n, m, 0)
     print(res)
     print(len(res))
