7c1199b42f584fc8b6c8ebe6ae43d8e275e5fbb7
Kim Jihoon <max.kim@ebridge-world.com>
2022-07-27 17:59:59 +0900

    바둑이 승차 cut edge

--- 바둑이_승차.py
+++ 바둑이_승차.py
@@ -1,26 +1,25 @@
+import sys
 
-
-
-def DFS(n, total, check_total):
-    global res
-    if sum_nums - check_total + total < res:
+# cut-edge-through
+def DFS(L, weight, weight_sum, check_sum):
+    if sum(nums) - check_sum + weight_sum < res[0]:
         return
-    elif total > _max:
+    elif weight_sum > weight:
         return
-    elif n == cnt:
-        if total > res:
-            res = total
+    elif L == len(nums):
+        if res[0] < weight_sum:
+           res[0] = weight_sum
     else:
-        DFS(n + 1, total + nums[n], check_total + nums[n])
-        DFS(n + 1, total, check_total + nums[n])
-
+        DFS(L + 1, weight, weight_sum + nums[L], check_sum + nums[L])
+        DFS(L + 1, weight, weight_sum, check_sum + nums[L])
 
 if __name__ == '__main__':
-    # main script 에 생성되면 전역변수
-    cnt = 5
-    _max = 259
+    weight = 259
     nums = [81, 58, 42, 33, 61]
-    sum_nums = sum(nums)
-    res = 0
-    DFS(0, 0, 0)
+    temp = [0] * len(nums)
+    weight_sum = 0
+    check_sum = 0
+    res = [0]
+    DFS(0, weight, weight_sum, check_sum)
+
     print(res)
