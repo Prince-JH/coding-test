4db282250b86303f3d4e152672141966d2d2ac2f
Kim Jihoon <max.kim@ebridge-world.com>
2022-07-30 23:27:43 +0900

    경로 탐색 DFS

--- 경로_탐색.py
+++ 경로_탐색.py
@@ -1,8 +1,6 @@
 import sys
 def DFS(v, n):
-    global cnt
     if v == n - 1:
-        cnt += 1
         for p in path:
             print(p, end=' ')
         print()
@@ -10,23 +8,21 @@ def DFS(v, n):
         for i in range(n):
             if matrix[v][i] == 1 and ch[i] == 0:
                 ch[i] = 1
+                # print('ch:', ch)
                 path.append(i + 1)
                 DFS(i, n)
-                path.pop()
                 ch[i] = 0
-
+                path.pop()
 
 if __name__ == '__main__':
     n = 5
-    matrix = [[0] * n for _ in range(n)]
-    ch = [0] * n
-
+    matrix = [[0] * n for i in range(n)]
     _input = [[1, 2], [1, 3], [1, 4], [2, 1], [2, 3], [2, 5], [3, 4], [4, 2], [4, 5]]
     for i in _input:
         matrix[i[0] - 1][i[1] - 1] = 1
-
+    print(matrix)
     path = [1]
-    cnt = 0
+    ch = [0] * n
     ch[0] = 1
     DFS(0, n)
-    print(cnt)
+
