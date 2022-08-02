9719b1fd99a8c8f34a211e07ca1dcf7c8b126dd5
Kim Jihoon <max.kim@ebridge-world.com>
2022-08-03 00:40:19 +0900

    휴가

--- 휴가.py
+++ 휴가.py
@@ -1,15 +1,14 @@
+def DFS(v, n, score_sum):
+    if v == n:
+        if score_sum > res[0]:
+            res[0] = score_sum
+    else:
+        DFS(v + _input[v][0], n, score_sum + _input[v][1])
+        DFS(v + 1, n, score_sum)
+
+
 if __name__ == '__main__':
-    num = '5276823'
-    n = 3
-    cnt = 0
-    stack = list()
-    stack.append(num[0])
-
-    for i in range(1, len(num)):
-        while len(stack) > 0 and stack[-1] < num[i] and cnt < n:
-            stack.pop()
-            cnt += 1
-        stack.append(num[i])
-
-    print(stack)
-    print(''.join(stack))
+    _input = [(3, 20), (2, 15), (3, 50), (3, 25), (2, 20), (2, 30), (1, 10)]
+    res = [0]
+    DFS(0, len(_input), 0)
+    print(res)
