599c07c96bb86155e4ce21abe18bfa880675a56e
Kim Jihoon <max.kim@ebridge-world.com>
2022-08-08 15:05:36 +0900

    동전 바꿔주기

--- 동전_바꿔주기.py
+++ 동전_바꿔주기.py
@@ -1,25 +1,24 @@
-def DFS(n):
-    print(n)
-    if n < 0:
+def DFS(v, n, amount):
+    if amount < 0:
         return
-    elif n == 0:
-        for i in range(len(ch)):
-            res.append(ch[:])
+    elif amount == 0:
+        res.append(0)
+    elif v == n:
+        return
     else:
-        for i in range(len(coins)):
-            if ch[i] < coins[i][1]:
-                ch[i] += 1
-                n -= coins[i][0]
-                DFS(n)
-                ch[i] -= 1
-                n += coins[i][0]
-            DFS(n)
+        for i in range(coins[v][1] + 1):
+            DFS(v + 1, n, amount - coins[v][0] * i)
+                # ch[i] -= 1
+                # amount += coins[i][0]
+                # DFS(v + 1, n, amount)
 
 
 if __name__ == '__main__':
-    amount = 20
-    coins = [(5, 3), (10, 2), (1, 5)]
+    amount = 486
+    coins = [(1, 10), (3, 9), (7, 9), (12, 7), (13, 9), (25, 7), (47, 5)]
+    n = len(coins)
     ch = [0] * len(coins)
     res = list()
     # print(nums)
-    DFS(amount)
+    DFS(0, n, amount)
+    print(len(res))
