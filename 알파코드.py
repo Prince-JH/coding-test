bc42ce1fb3f3a2dd2c326eb000cbc53b1af6084c
Kim Jihoon <max.kim@ebridge-world.com>
2022-08-11 15:25:21 +0900

    알파코드

--- 알파코드.py
+++ 알파코드.py
@@ -1,17 +1,16 @@
 def DFS(L, n):
     global cnt
     if L == n:
-        for x in res:
-            print(x, end='')
+        for r in res:
+            print(r, end=' ')
         print()
         cnt += 1
-        return
     else:
         for i in range(L, n):
-            # print(nums[L: i + 1])
             if nums[L] != '0' and 1 <= int(nums[L: i + 1]) <= 26:
                 res.append(chr(int(nums[L: i + 1]) + 64))
-                DFS(i + 1, n)
+                print(res)
+                DFS(L + 1, n)
                 res.pop()
 
 
