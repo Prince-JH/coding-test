6a72f0a2d7a3419de040a25d57e31e7ff9ed291b
Kim Jihoon <max.kim@ebridge-world.com>
2022-08-16 00:36:37 +0900

    미로의 최단거리 통로

---
+++ 미로의_최단거리_통로.py
@@ -0,0 +1,33 @@
+if __name__ == '__main__':
+    maze = [[0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 1, 1, 1, 1, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+            [1, 1, 0, 1, 0, 1, 1],
+            [1, 1, 0, 1, 0, 0, 0],
+            [1, 0, 0, 0, 1, 0, 0],
+            [1, 0, 1, 0, 0, 0, 0]]
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
+    queue = list()
+    n = len(maze)
+    dis = [[-1] * n for _ in range(n)]
+    dis[0][0] = 0
+    queue.append((0, 0))
+    res = list()
+
+    while queue:
+        # print(queue)
+        size = len(queue)
+        for i in range(size):
+            temp = queue.pop(0)
+            temp_dis = dis[temp[0]][temp[1]]
+            for j in range(4):
+                x = temp[0] + dx[j]
+                y = temp[1] + dy[j]
+                if 0 <= x < n and 0 <= y < n and maze[x][y] == 0 and dis[x][y] == -1:
+                    dis[x][y] = temp_dis + 1
+                    queue.append((x, y))
+        # print(size)
+    for x in dis:
+        print(x)
+
