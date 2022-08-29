83b50164628414c67dac1838879400925ba0e61a
Kim Jihoon <max.kim@ebridge-world.com>
2022-08-30 01:07:15 +0900

    피자 배달 거리

--- 피자_배달_거리.py
+++ 피자_배달_거리.py
@@ -2,31 +2,45 @@ import sys
 from collections import deque
 
 
-def DFS(y, x):
-    if y == 0:
-        print('!!!END!!!', x)
-        sys.exit()
+def DFS(L, s):
+    global res
+    if L == m:
+        sum = 0
+        for j in range(len(houses)):
+            x1 = houses[j][0]
+            y1 = houses[j][1]
+            dis = float("inf")
+            for k in combinations:
+                x2 = pizzas[k][0]
+                y2 = pizzas[k][1]
+                dis = min(dis, abs(x1 - x2) + abs(y1 - y2))
+            sum += dis
+        if sum < res:
+            res = sum
     else:
-        for i in range(3):
-            board[y][x] = 0
-            xx = x + dx[i]
-            yy = y + dy[i]
-            if 0 <= yy < n and 0 <= xx < n and board[yy][xx] >= 1:
-                DFS(yy, xx)
-                board[y][x] = 1
-                break
+        for i in range(s, len(pizzas)):
+            combinations[L] = i
+            DFS(L + 1, i + 1)
 
 
 if __name__ == '__main__':
-    n = 10
-    board = [[] for _ in range(10)]
     f = open("/Users/jihoon/Downloads/pythonalgorithm_formac/섹션 7/17. 피자 배달 거리/in2.txt", 'r')
-    for i in range(10):
+    n, m = map(int, f.readline().split())
+    board = [[] for _ in range(n)]
+    for i in range(n):
         line = f.readline()
         board[i] = list(map(int, line.split()))
     f.close()
-    dx = [-1, 1, 0]
-    dy = [0, 0, -1]
+    houses = list()
+    pizzas = list()
+    combinations = [0] * m
+    res = float("inf")
     for i in range(n):
-        if board[9][i] == 2:
-            DFS(9, i)
+        for j in range(n):
+            if board[i][j] == 1:
+                houses.append((i, j))
+            elif board[i][j] == 2:
+                pizzas.append((i, j))
+
+    DFS(0, 0)
+    print(res)
